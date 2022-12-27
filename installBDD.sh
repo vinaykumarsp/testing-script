725b871c19e420f58935ce97d4110b00800ddfa3
shbharath46 <121396057+shbharath46@users.noreply.github.com>
2022-12-27 19:18:13 +0530

    Create installBDD.sh
    shell script written to install and run behave

---
+++ installBDD.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "installing behave"
+python_install()
+{
+       sudo apt install python3 -y
+       if [ $? -ne 0 ]; then
+                echo "error while installing behave"
+       fi
+       echo "python installed successfully"
+}
+behave_install()
+{
+       sudo apt install python3-behave -y
+       if [ $? -ne 0 ]; then
+                echo "error while installing behave"
+       fi
+       echo "behave installed successfully"
+}
+cleanup_workspace()
+{
+       echo "removing old beahve files"
+       sudo rm -r /var/lib/jenkins/workspace/testbehave
+       if [ $? -ne 0 ]; then
+                echo "error while removing behave files"
+       fi
+       echo "behave files removed successfully"
+}
+behave_steps()
+{
+        echo "steps to run BDD"
+        sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
+        sudo cp -r ./test.feature /var/lib/jenkins/workspace/testbehave/features
+        sudo cp -r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
+        cd /var/lib/jenkins/workspace/testbehave
+        behave
+        
+}
+python_install
+behave_install
+cleanup_workspace
+behave_steps
