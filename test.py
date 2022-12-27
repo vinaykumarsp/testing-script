c244b391a48f1ffff84f3d2cf084458b447cb4e5
mhm1nh3 <114675830+mhm1nh3@users.noreply.github.com>
2022-12-27 22:22:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ def step_impl(context):
 def step_impl(context):
   assert True is not False
 
-@then(u'behave will test it for us')
+@then('behave will test it for us')
 def step_impl(context):
   assert context.failed is False
