f1c5ccb04208e4cc77c71c87c20b887e2fa82b75
vinaykumarsp <114675772+vinaykumarsp@users.noreply.github.com>
2022-12-28 16:00:46 +0530

    Update behave.sh

--- behave.sh
+++ behave.sh
@@ -31,7 +31,9 @@ behave_steps()
         sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
         sudo cp -r ./test.feature /var/lib/jenkins/workspace/testbehave/features
         sudo cp -r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
-        behave     
+        cd /var/lib/jenkins/workspace/testbehave
+        behave
+        
 }
 python_install
 behave_install
