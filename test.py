bdf33812cdcde5178ae0a031a66637b95b89e4c1
vinaykumarsp <114675772+vinaykumarsp@users.noreply.github.com>
2022-12-28 16:04:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ def step_impl(context):
 def step_impl(context):
   assert True is not False
 
-@then('behave will test it for us')
+@then(u'behave will test it for us')
 def step_impl(context):
   assert context.failed is False
